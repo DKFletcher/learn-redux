b23a5d9b47967827356517332776d80e1f4de536
DKFletcher <dfletcher@whatmotivateslearning.com>
2020-07-19 10:05:26 +0100

    Simple store, dispatch, action and reducer added to a counter.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+
+
+// STORE
+
+
+// ACTION
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+      case "DECREMENT":
+        return state - 1;
+  }
+}
+
+let store = createStore(counter);
+store.subscribe( ()=> console.log(store.getState()))
+
+// DISPATCH
+store.dispatch(increment())
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
